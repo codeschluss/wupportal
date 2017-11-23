2e2ca0c4c365af702c0b4ee70e6e1ef696c118b8
vetemi <valmir.e@gmx.de>
2017-11-23 16:27:05 +0100

    updated package json to current versions

--- backend/config/schema/app.sql
+++ backend/config/schema/app.sql
@@ -72,7 +72,7 @@ CREATE TABLE `addresses` (
 
 CREATE TABLE `users` (
 	`id` CHAR(36) NOT NULL PRIMARY KEY,
-	`admin` BOOLEAN DEFAULT FALSE,
+	`superuser` BOOLEAN DEFAULT FALSE,
 	`username` VARCHAR(255) UNIQUE NOT NULL,
 	`password` VARCHAR(255) NOT NULL,
 	`fullname` VARCHAR(255),

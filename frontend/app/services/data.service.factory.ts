b34da8878a5f3924d8f8db6358c899d712d3af93
vetemi <valmir.e@gmx.de>
2017-11-26 17:59:32 +0100

    removed unused import

--- frontend/app/services/data.service.factory.ts
+++ frontend/app/services/data.service.factory.ts
@@ -2,7 +2,6 @@
 import { InjectionToken } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DataService } from 'app/services/data.service';
-import { Model } from 'app/models/model';
 
 export function dataServiceFactory(repository: string): any {
 	return (http: HttpClient) => {

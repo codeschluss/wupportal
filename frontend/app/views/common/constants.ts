487f1d594fca9cbcd5917688fc55d506daf41850
vetemi <valmir.e@gmx.de>
2017-11-27 16:40:21 +0100

    fixed lint issues and added table section

--- frontend/app/views/common/constants.ts
+++ frontend/app/views/common/constants.ts
@@ -3,49 +3,60 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class Constants {
 
-	public back = 'Zurück';
-	public newElement = 'Neu';
-	public cancel = 'Abbrechen';
-	public next = 'Näachste';
-	public previous = 'Vorherige';
-	public items = 'Einträge';
-	public of = 'von';
-	public per = 'pro';
+	// --------------------------
+	// LABELS
+	// --------------------------
 
-	public login = 'anmelden';
-	public userName = 'Username';
-	public password = 'Passwort';
+	public back: string = 'Zurück';
+	public newElement: string = 'Neu';
+	public cancel: string = 'Abbrechen';
+	public next: string = 'Näachste';
+	public previous: string = 'Vorherige';
+	public items: string = 'Einträge';
+	public of: string = 'von';
+	public per: string = 'pro';
 
-	public filter = 'Filter';
-	public configuration = 'Einstellungen';
+	public login: string = 'anmelden';
+	public userName: string = 'Username';
+	public password: string = 'Passwort';
 
-	public delete = 'Löschen';
-	public change = 'Ändern';
-	public edit = 'Bearbeiten';
-	public deleteMessage = 'Möchten Sie den folgenden Eintrag wirklich löschen?';
+	public filter: string = 'Filter';
+	public configuration: string = 'Einstellungen';
 
-	public website = 'Webseite';
-	public description = 'Beschreibung';
-	public nameString = 'Name';
-	public phone = 'Telefon';
-	public mail = 'e-Mail';
-	public address = 'Adresse';
-	public street = 'Straße';
-	public postalCode = 'PLZ';
-	public houseNumber = 'Hausnummer';
-	public longitude = 'Längengrad';
-	public latitude = 'Breitengrad';
-	public dates = 'Termine';
-	public tags = 'Tags';
-	public targetGroups = 'Zielgruppen';
+	public delete: string = 'Löschen';
+	public change: string = 'Ändern';
+	public edit: string = 'Bearbeiten';
+	public deleteMessage: string = 'Möchten Sie den folgenden Eintrag wirklich löschen?';
 
-	public organisations = 'Organisationen';
-	public organisation = 'Organisation';
-	public users = 'Benutzer';
-	public user = 'Benutzer';
-	public activities = 'Aktivitäten';
-	public activity = 'Aktivität';
+	public website: string = 'Webseite';
+	public description: string = 'Beschreibung';
+	public nameString: string = 'Name';
+	public phone: string = 'Telefon';
+	public mail: string = 'e-Mail';
+	public address: string = 'Adresse';
+	public street: string = 'Straße';
+	public postalCode: string = 'PLZ';
+	public houseNumber: string = 'Hausnummer';
+	public longitude: string = 'Längengrad';
+	public latitude: string = 'Breitengrad';
+	public dates: string = 'Termine';
+	public tags: string = 'Tags';
+	public targetGroups: string = 'Zielgruppen';
 
-	public error = 'Fehler';
+	public organisations: string = 'Organisationen';
+	public organisation: string = 'Organisation';
+	public users: string = 'Benutzer';
+	public user: string = 'Benutzer';
+	public activities: string = 'Aktivitäten';
+	public activity: string = 'Aktivität';
+
+	public error: string = 'Fehler';
+
+	// --------------------------
+	// TABLE
+	// --------------------------
+
+	public defaultPageSize: number = 25;
+	public pageSizeOptions: Array<number> = [5, 10, 25, 50, 100];
 
 }

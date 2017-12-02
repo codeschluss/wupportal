673ea45b225bf2f12b03a1c033df6ed556f43eef
vetemi <valmir.e@gmx.de>
2017-12-02 23:48:48 +0100

    Cleanup

--- backend/config/bootstrap.php
+++ backend/config/bootstrap.php
@@ -207,7 +207,6 @@ Type::build('timestamp')
  * Plugin::load('Migrations'); //Loads a single plugin named Migrations
  *
  */
-Plugin::load('ADmad/JwtAuth');
 
 /*
  * Only try to load DebugKit in development mode

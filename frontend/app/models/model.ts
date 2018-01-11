fbe7c90258f9ff3fd47554ba5122f34df82b3687
vetemi <valmir.e@gmx.de>
2018-01-11 16:55:51 +0100

    Removed Created/modified on client side

--- frontend/app/models/model.ts
+++ frontend/app/models/model.ts
@@ -1,13 +1,9 @@
 export abstract class Model {
 
 	public id: string = '';
-	public created: Date = new Date(Date.now());
-	public modified: Date = new Date(Date.now());
 
 	constructor(id: string = '') {
 		this.id = id && id || '';
-		this.created = new Date(Date.now());
-		this.modified = new Date(Date.now());
 	}
 
 }

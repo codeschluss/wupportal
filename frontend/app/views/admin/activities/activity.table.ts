205eb1c8fb856e77fa0ae397a797356f61f5fd05
Peter Seipel <pseipel1@uni-koeln.de>
2018-03-28 14:57:50 +0200

    use approvedProvider of currentUser to check visibility of new Activity Button

--- frontend/app/views/admin/activities/activity.table.ts
+++ frontend/app/views/admin/activities/activity.table.ts
@@ -51,12 +51,7 @@ export class ActivityTableComponent extends AbstractTableComponent implements On
 		if (this.actionsVisible()) {
 			this.showNewButton = true;
 		} else {
-			let tmpProvs = 0;
-			this.providerService
-				.getByUser(this.authService.currentUser.id)
-				.subscribe(providers => providers.map(provider => {
-					if (provider.approved || this.showNewButton) { tmpProvs++; this.showNewButton = true; }
-				}));
+			this.showNewButton = this.authService.currentUser.approvedProvider;
 		}
 	}
 

4dc89c11cf002004ddcc7010d1eb314be60775f8
Phil <me@phil.re>
2018-05-04 14:23:20 +0200

    Switch getAll method

--- frontend/app/services/data.resolver.ts
+++ frontend/app/services/data.resolver.ts
@@ -1,7 +1,8 @@
 
-import { Inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 
+import { ActivityService } from 'app/services/activity.service';
 import { DataService } from 'app/services/data.service';
 
 @Injectable()
@@ -10,10 +11,15 @@ export class DataResolver implements Resolve<any> {
 	constructor(private dataService: DataService) { }
 
 	resolve(route: ActivatedRouteSnapshot): any {
-		const uuid: string = route.paramMap.get('uuid');
+		let method = 'getAll';
+		const uuid = route.paramMap.get('uuid');
 
-		return uuid
-			? this.dataService.get(uuid)
-			: this.dataService.getAll();
+		switch (this.dataService.constructor) {
+			case ActivityService:
+				method = 'getByMapfilter';
+				break;
+		}
+
+		return uuid ? this.dataService.get(uuid) : this.dataService[method]();
 	}
 }

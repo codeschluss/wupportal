b04b696a38f9408587066a7e99d7d0ddf04897d0
vetemi <valmir.e@gmx.de>
2018-07-29 17:19:44 +0200

    Added pagable API methods with custom search params

--- server/wupportal/src/main/java/de/codeschluss/wupportal/repository/IActivityRepository.java
+++ server/wupportal/src/main/java/de/codeschluss/wupportal/repository/IActivityRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.domain.Pageable;
 
 import de.codeschluss.wupportal.model.Activity;
 
@@ -13,5 +14,7 @@ import de.codeschluss.wupportal.model.Activity;
 public interface IActivityRepository extends JpaRepository<Activity, String> {
 	
 	Optional<List<Activity>> findByProviderIdIn(@Param("providerId") List<String> providerId);
+	
+	Optional<List<Activity>> findPagedByProviderIdIn(@Param("providerId") List<String> providerId, Pageable page);
 
 }
--- server/wupportal/src/main/java/de/codeschluss/wupportal/repository/IProviderRepository.java
+++ server/wupportal/src/main/java/de/codeschluss/wupportal/repository/IProviderRepository.java
@@ -3,6 +3,7 @@ package de.codeschluss.wupportal.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -13,6 +14,8 @@ import de.codeschluss.wupportal.model.Provider;
 public interface IProviderRepository extends JpaRepository<Provider, String> {
 	
 	Optional<List<Provider>> findByOrganisationId(@Param("organisationId") String organisationId);
+	
+	Optional<List<Provider>> findPagedByOrganisationId(@Param("organisationId") String organisationId, Pageable page);
 	
 	Optional<List<Provider>> findByUserId(@Param("userId") String userId);
 

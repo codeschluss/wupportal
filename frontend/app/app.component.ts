2af7176d53270699a0922e01cb6ae1308329869f
Peter Seipel <Pseipel@web.de>
2018-09-18 10:38:38 +0200

    setting title dynamically from db

--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ConfigurationService } from 'app/services/data.service.factory';
+import { DataService } from 'app/services/data.service';
+
 
 @Component({
 	selector: 'app-root',
@@ -6,4 +10,18 @@ import { Component } from '@angular/core';
 	templateUrl: 'app.component.html'
 })
 
-export class AppComponent { }
+export class AppComponent {
+	public constructor(
+		private titleService: Title,
+		@Inject(ConfigurationService) private configurationService: DataService) {
+		this.configurationService.getAll().subscribe(
+			items => { this.setTitle(items.find(item => item.item === 'portalName').value); }
+		);
+	}
+
+	public setTitle(newTitle: string): void {
+		console.log('newTitle: ', newTitle);
+		this.titleService.setTitle(newTitle);
+	}
+
+}

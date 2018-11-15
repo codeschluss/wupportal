f50275a7cb6b9c03f19e30b8fba8c2aaddf364ba
Phil <me@phil.re>
2018-11-15 21:20:50 +0100

    Regenerate API docs/code

--- client/src/core/api/models/resource-activity-entity.ts
+++ client/src/core/api/models/resource-activity-entity.ts
@@ -2,13 +2,10 @@
 import { Link } from './link';
 export interface ResourceActivityEntity {
   _links?: Array<Link>;
-  addressId?: string;
-  categoryId?: string;
   created?: string;
   description?: string;
   id?: string;
   modified?: string;
   name?: string;
-  organisationId?: string;
   showUser?: boolean;
 }

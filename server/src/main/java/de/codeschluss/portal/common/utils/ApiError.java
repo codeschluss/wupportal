5d6e2705ac0af0a0d7af312a4bd7ab39321f6a7e
Phil <me@phil.re>
2018-11-15 22:09:01 +0100

    Improve error handling

--- server/src/main/java/de/codeschluss/portal/common/utils/ApiError.java
+++ server/src/main/java/de/codeschluss/portal/common/utils/ApiError.java
@@ -4,17 +4,14 @@ import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import lombok.Data;
 
 @Data
 public class ApiError {
 
-	private int status;
-	
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
 	private LocalDateTime timestamp;
+
+	private int status;
 	
 	private String error;
 	
@@ -22,7 +19,7 @@ public class ApiError {
 	
 	public ApiError(HttpStatus status, String error, String message) {
 		this.status = status.value();
-		this.error = message;
+		this.error = error;
 		this.message = message;
 		this.timestamp = LocalDateTime.now();
 	}

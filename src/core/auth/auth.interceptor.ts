f700b583dcf01d027fb28b09c7626630c2351891
Phil <me@phil.re>
2018-11-17 04:53:49 +0100

    Subscribe to session via provider

--- src/core/auth/auth.interceptor.ts
+++ src/core/auth/auth.interceptor.ts
@@ -1,11 +1,18 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { SessionProvider } from '../session/session.provider';
 
 @Injectable({ providedIn: 'root' })
 export class AuthInterceptor implements HttpInterceptor {
 
   public bearer: string;
+
+  public constructor(
+    private session: SessionProvider
+  ) {
+    this.session.subscribe((next) => this.bearer = next.bearer);
+  }
 
   public intercept(req: HttpRequest<any>, next: HttpHandler):
     Observable<HttpEvent<any>> {

61d3df17d20af8511ad999ff246fa41f8fdd5753
PSeipel <Pseipel@web.de>
2018-11-18 21:51:28 +0100

    organisation and activity views mod

--- src/views/public/public.router.ts
+++ src/views/public/public.router.ts
@@ -3,6 +3,9 @@ import { RouterModule } from '@angular/router';
 import { PublicComponent } from './public.component';
 import { AboutComponent } from './layout/about.component';
 import { ActivityListComponent } from './activity/activity.list.component';
+import { ActivityViewComponent } from './activity/activity.view.component';
+import { LayoutComponent } from '../layout/layout.component';
+import { OrganisationListComponent } from './organisation/organisation.list.component';
 
 const PublicProviders = [
 ];
@@ -15,19 +18,41 @@ const PublicRoutes = [
     path: 'home',
     // resolve: PublicResolvers,
     component: AboutComponent,
-    children: []
   },
+
   {
-    path: 'list',
+    path: 'activities',
     // resolve: PublicResolvers,
-    component: ActivityListComponent,
-    children: []
+    children: [
+      {
+        path: 'view/:id',
+        component: ActivityViewComponent
+      },
+      {
+        path: 'list', 
+        component: ActivityListComponent,
+      },
+      {
+        path: '', 
+        component: ActivityListComponent,
+      } 
+    ]
+  },
+
+  {
+  path: 'organisations',
+  // resolve: PublicResolvers,
+  children: [
+      {
+        path: 'list', 
+        component: OrganisationListComponent,
+      }
+    ]
   },
   {
     path: '',
     // resolve: PublicResolvers,
-    component: PublicComponent,
-    children: []
+    component: AboutComponent,
   },
   {
     path: '**',
@@ -38,7 +63,12 @@ const PublicRoutes = [
 
 @NgModule({
   exports: [RouterModule],
-  imports: [RouterModule.forChild(PublicRoutes)],
+  imports: [RouterModule.forChild([{
+    path: '',
+    children: PublicRoutes,
+    component: LayoutComponent,
+    resolve: PublicResolvers,
+  }])], 
   providers: PublicProviders
 })
 

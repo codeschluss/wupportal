2fba03536ab36573bd0059c5160e4dca63135d72
PSeipel <Pseipel@web.de>
2018-11-21 18:16:40 +0100

    mod activity list view

--- src/client.ts
+++ src/client.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { library as fontawesome } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { ClientModule } from 'src/client.module';
+import 'hammerjs';
 
 enableProdMode();
 fontawesome.add(fas);

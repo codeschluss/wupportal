24a1a24b0cb0a6e909ee37c05791df4238de3155
PSeipel <Pseipel@web.de>
2018-11-21 16:12:00 +0100

    fixed broken carousel and about component

--- src/views/public/layout/about.component.ts
+++ src/views/public/layout/about.component.ts
@@ -5,8 +5,7 @@ import { CategoryModel } from '../../../core/models/category.model';
 import { ScheduleModel } from '../../../core/models/schedule.model';
 import { SuburbModel } from '../../../core/models/suburb.model';
 import { TargetGroupModel } from '../../../core/models/target-group.model';
-
-
+import { OrganisationModel } from 'src/core/models/organisation.model';
 
 
 @Component({
@@ -27,63 +26,82 @@ export class AboutComponent {
   constructor() {
     for (let i = 0; i < 12; i++) {
       this.activities.push(
-        this.buildTestActions());
+        this.buildTestActivity());
     }
   }
 
 
-  buildTestActions(): ActivityModel {
-
-    const actOne: any = new ActivityModel;
+  buildTestActivity(): ActivityModel {
+    const actOne = new ActivityModel;
+    actOne.id="testActivity";
     actOne.name = 'FakeActivity';
     actOne.description = 'This is just a FakeActivity to show'
       + 'how this could look like.';
-
-    const testAddress: any = new AddressModel();
+    const testAddress = new AddressModel();
     testAddress.street = 'samplestreet';
     testAddress.houseNumber = '42a';
     testAddress.latitude = 51.00;
     testAddress.longitude = 7.00;
     testAddress.postalCode = '63628';
-    const testSubUrb: any = new SuburbModel();
+
+    const testSubUrb = new SuburbModel();
     testSubUrb.name = 'Elberfeld';
-    testAddress.suburb = testSubUrb;
+    testSubUrb.id = '1';
+
+    testAddress.suburb = new Promise<SuburbModel>((resolve, reject) => {
+      resolve(testSubUrb);
+    });
+
     testAddress.place = 'SampleCity';
-    testAddress.suburb.id = '1';
-
-    actOne.address = testAddress;
-
-    const category: any = new CategoryModel;
+    actOne.address = new Promise<AddressModel>((resolve, reject) => {
+      resolve(testAddress);
+    });
+    
+    const category = new CategoryModel;
     category.name = 'party';
-    category.color = 'green';
-    actOne.category = category;
+    category.color = 'blue';
 
-    const target_group: any = new TargetGroupModel;
+    actOne.category = new Promise<CategoryModel>((resolve, reject) => {
+      resolve(category);
+    });
+
+    const target_group = new TargetGroupModel;
     target_group.name = 'youth';
-    actOne.targetGroups = [target_group];
+    const targetGroups = [target_group];
 
-    const schedule: any = new ScheduleModel;
+    actOne.targetGroups = new Promise<TargetGroupModel[]>((resolve, reject) => {
+      resolve(targetGroups);
+    });
+    
+    const schedule = new ScheduleModel;
     schedule.startDate = new Date().toUTCString();
     schedule.endDate = new Date().toUTCString();
-    actOne.schedules = [schedule];
-
-    // const organisation = new OrganisationModel;
-    // organisation.name = 'testOrganisation';
-    // actOne.provider.organisation = organisation;
+    const schedules = [schedule];
 
     const firstDate = new ScheduleModel;
     firstDate.startDate = new Date().toISOString();
     firstDate.endDate =
       new Date(new Date(firstDate.startDate).getDate() + 1).toISOString();
 
-    const secondDate = new ScheduleModel;
+      const secondDate = new ScheduleModel;
     secondDate.startDate = new Date(new Date().getDate() + 7).toISOString();
     secondDate.endDate =
       new Date(new Date(secondDate.startDate).getDate() + 1).toISOString();
 
-    actOne.schedules.push(firstDate);
-    actOne.schedules.push(secondDate);
+      schedules.push(firstDate);
+    schedules.push(secondDate);
+    
+    actOne.schedules = new Promise<ScheduleModel[]>((resolve, reject) => {
+      resolve(schedules);
+    });
+
+    const organisation = new OrganisationModel;
+    organisation.name = 'testOrganisation';
+    actOne.organisation = new Promise<OrganisationModel>((resolve, reject) => {
+      resolve(organisation);
+    });
+    
     return actOne;
-  }
+}
 
 }

d104afc25501d80bf4e3484e7e64ceba02b39d0b
Phil <me@phil.re>
2018-11-22 00:41:19 +0100

    Refactoring, (re)implemented auth/refresh, minor tweaks

--- client/src/core/session/session.provider.ts
+++ client/src/core/session/session.provider.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { LocalStorage } from '@ngx-pwa/local-storage';
 import { BehaviorSubject, Subscription } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 import { AccessTokenModel } from '../auth/access-token.model';
+import { RefreshTokenModel } from '../auth/refresh-token.model';
 import { TokenService } from '../auth/token.service';
 import { SessionModel } from './session.model';
 import { SessionResolver } from './session.resolver';
@@ -13,39 +14,59 @@ export class SessionProvider {
   private session: BehaviorSubject<SessionModel>;
 
   public constructor(
-    private resolver: SessionResolver,
     private service: TokenService,
-    private storage: LocalStorage
+    private storage: LocalStorage,
+
+    resolver: SessionResolver
   ) {
-    (this.session = new BehaviorSubject<SessionModel>(this.resolver.session))
+    (this.session = new BehaviorSubject<SessionModel>(resolver.session))
       .subscribe(session => this.storage.setItemSubscribe('session', session));
   }
 
+  public like(id: string): void {
+    if (!this.session.value.likes.includes(id)) {
+      this.session.next(Object.assign(this.session.value, {
+        likes: this.session.value.likes.concat(id)
+      }));
+    }
+  }
+
   public login(username: string, password: string): Promise<any> {
-    return this.service.authLoginResponse(username, password).pipe(
-      map((response) => this.update(response.statusText, response.body))
+    return this.service.apiLoginResponse(username, password).pipe(
+      tap((response) => this.update(response.body))
     ).toPromise();
   }
 
   public logout(): void {
-    this.update('', new AccessTokenModel());
+    this.update({
+      access: new AccessTokenModel(),
+      refresh: new RefreshTokenModel()
+    });
   }
 
   public refresh(): Promise<any> {
-    return this.service.authRefreshResponse().pipe(
-      map((response) => this.update(response.statusText, response.body))
+    return this.service.apiRefreshResponse().pipe(
+      tap((response) => this.update(response.body))
     ).toPromise();
   }
 
-  public subscribe(next?: (value: SessionModel) => void): Subscription {
+  public subscribe(next: (value: SessionModel) => void): Subscription {
     return this.session.subscribe((value) => next(value));
   }
 
-  private update(bearer: string, token: AccessTokenModel): void {
+  private update(tokens: any): void {
     this.session.next(Object.assign(this.session.value, {
-      bearer: bearer,
-      token: token
+      accessToken: tokens.access || this.session.value.accessToken,
+      refreshToken: tokens.refresh || this.session.value.refreshToken
     }));
+  }
+
+  private worker(): void {
+    const accessExp = this.session.value.accessToken.exp - 10;
+    const refreshExp = this.session.value.refreshToken.exp - 10;
+
+    refreshExp > 0 && refreshExp < Date.now() / 1000 ? this.logout() :
+      setTimeout(() => this.refresh().then(() => this.worker()), accessExp);
   }
 
 }

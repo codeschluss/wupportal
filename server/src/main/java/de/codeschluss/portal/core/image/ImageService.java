bda313fa3e02cc4fff69a3648f5f13777de46d98
vetemi <valmir.e@gmx.de>
2018-12-09 23:04:45 +0100

    fixed scaling issue

--- server/src/main/java/de/codeschluss/portal/core/image/ImageService.java
+++ server/src/main/java/de/codeschluss/portal/core/image/ImageService.java
@@ -1,12 +1,13 @@
 package de.codeschluss.portal.core.image;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,26 +19,34 @@ import org.springframework.web.multipart.MultipartFile;
  */
 @Service
 public class ImageService {
-  
+
   private final ImageConfiguration config;
-  
+
   public ImageService(ImageConfiguration config) {
     this.config = config;
   }
-  
+
   /**
    * Resize.
    *
-   * @param imageFile the image file
+   * @param imageFile
+   *          the image file
    * @return the byte[]
-   * @throws IOException Signals that an I/O exception has occurred.
+   * @throws IOException
+   *           Signals that an I/O exception has occurred.
    */
   public byte[] resize(MultipartFile imageFile) throws IOException {
     BufferedImage imageBuff = ImageIO.read(imageFile.getInputStream());
-    imageBuff = Scalr.resize(
-        imageBuff, Scalr.Method.ULTRA_QUALITY, config.getMaxWidth(), config.getMaxWidth());
-    return ((DataBufferByte) imageBuff.getData().getDataBuffer()).getData();
+    Image image = imageBuff.getScaledInstance(config.getMaxWidth(), -1, Image.SCALE_SMOOTH);
+
+    BufferedImage bimage = new BufferedImage(image.getWidth(null), image.getHeight(null),
+        BufferedImage.TYPE_3BYTE_BGR);
+
+    Graphics2D neGraphic = bimage.createGraphics();
+    neGraphic.drawImage(image, 0, 0, null);
+    neGraphic.dispose();
+
+    return ((DataBufferByte) bimage.getData().getDataBuffer()).getData();
   }
-  
-  
+
 }

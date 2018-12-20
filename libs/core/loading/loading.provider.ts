4b2a0353d8e235c34df41f0d81831e4f294872a5
Phil <me@phil.re>
2018-12-20 02:50:02 +0100

    [Client/core] Update loading indicator (use counter instead of bool)

--- libs/core/loading/loading.provider.ts
+++ libs/core/loading/loading.provider.ts
@@ -5,23 +5,23 @@ import { BehaviorSubject, Subscription } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class LoadingProvider {
 
-  private loads: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  private loads: BehaviorSubject<number> = new BehaviorSubject(0);
 
   private requests: HttpRequest<any>[] = [];
 
   public enqueue(request: HttpRequest<any>): void {
     this.requests.push(request);
-    this.loads.next(true);
+    this.loads.next(this.requests.length);
   }
 
   public finished(request: HttpRequest<any>): void {
     if (this.requests.includes(request)) {
-      this.requests = this.requests.filter((req) => req !== request);
-      this.loads.next(!!this.requests.length);
+      this.requests = this.requests.filter((r) => r !== request);
+      this.loads.next(this.requests.length);
     }
   }
 
-  public subscribe(next: (value: boolean) => void): Subscription {
+  public subscribe(next: (value: number) => void): Subscription {
     return this.loads.subscribe((value) => next(value));
   }
 

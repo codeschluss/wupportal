aa7e2d599e7bcf0c07885b89e84d4c4784f4a5d0
vetemi <valmir.e@gmx.de>
2019-01-04 21:47:08 +0100

    Fixed Approved filter in organisations

--- server/src/main/java/de/codeschluss/portal/components/organisation/OrganisationQueryBuilder.java
+++ server/src/main/java/de/codeschluss/portal/components/organisation/OrganisationQueryBuilder.java
@@ -113,28 +113,34 @@ public class OrganisationQueryBuilder extends QueryBuilder<QOrganisationEntity> 
    * @return the predicate
    */
   private Predicate searchFiltered(BooleanBuilder search, OrganisationQueryParam params) {
-    if (params.getApproved() != null && params.getApproved()) {
-      search.and(withApprovedOnly());
+    if (params.getApproved() != null) {
+      search.and(withApproved(params.getApproved()));
     }
-    String filter = prepareFilter(params.getFilter());
-    return search.and(
-        query.name.likeIgnoreCase(filter)
-        .or(likeDescription(filter))
-        .or(query.mail.likeIgnoreCase(filter))
-        .or(query.phone.likeIgnoreCase(filter))
-        .or(query.website.likeIgnoreCase(filter))
-        .or(query.address.houseNumber.likeIgnoreCase(filter))
-        .or(query.address.place.likeIgnoreCase(filter))
-        .or(query.address.street.likeIgnoreCase(filter)));
+    
+    if (params.getFilter() != null && !params.getFilter().isEmpty()) {
+      String filter = prepareFilter(params.getFilter());
+      search.and(
+          query.name.likeIgnoreCase(filter)
+          .or(likeDescription(filter))
+          .or(query.mail.likeIgnoreCase(filter))
+          .or(query.phone.likeIgnoreCase(filter))
+          .or(query.website.likeIgnoreCase(filter))
+          .or(query.address.houseNumber.likeIgnoreCase(filter))
+          .or(query.address.place.likeIgnoreCase(filter))
+          .or(query.address.street.likeIgnoreCase(filter)));
+    }
+    
+    return search;
   }
   
   /**
-   * With approved only.
+   * With approved.
    *
+   * @param approved the approved
    * @return the predicate
    */
-  private BooleanExpression withApprovedOnly() {
-    return query.approved.isTrue();
+  private BooleanExpression withApproved(boolean approved) {
+    return query.approved.eq(approved);
   }
 
   /**

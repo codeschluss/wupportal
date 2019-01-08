45a7d42027f7ab9740c99af2e67c02c96a375432
Pseipel <Pseipel@web.de>
2019-01-08 15:36:40 +0100

    loadingoverlay in client

--- src/client.component.ts
+++ src/client.component.ts
@@ -1,20 +1,25 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { LoadingProvider } from '@portal/core';
 import { ConfigurationProvider } from './realm/configuration/configuration.provider';
 
 @Component({
   selector: 'client-component',
   template: `
-      <router-outlet></router-outlet>
-      <loading-indicator></loading-indicator>
-  `
+  <main id="client" [class.disabled]="loadingProvider.value | async">
+    <router-outlet></router-outlet>
+    <loading-indicator></loading-indicator>
+  </main>
+  `,
+  styleUrls: ['client.component.scss']
 })
 
 export class ClientComponent {
 
   constructor(
     private titleService: Title,
-    private configProvider: ConfigurationProvider
+    private configProvider: ConfigurationProvider,
+    public loadingProvider: LoadingProvider
     ) {
       this.configProvider.readAll()
       .subscribe(configs => {

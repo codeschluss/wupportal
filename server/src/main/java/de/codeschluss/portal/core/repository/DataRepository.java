1a005f833d61c0d9a4b37767d32cee4c1a44a2f0
vetemi <valmir.e@gmx.de>
2019-01-27 23:11:21 +0100

    override findAll

--- server/src/main/java/de/codeschluss/portal/core/repository/DataRepository.java
+++ server/src/main/java/de/codeschluss/portal/core/repository/DataRepository.java
@@ -1,5 +1,7 @@
 package de.codeschluss.portal.core.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
@@ -12,4 +14,7 @@ import org.springframework.data.repository.NoRepositoryBean;
 @NoRepositoryBean
 public interface DataRepository<T>
     extends CrudRepository<T, String>, QueryableReadRepository<T> {
+  
+  @Override
+  List<T> findAll();
 }

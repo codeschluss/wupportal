a95cde7bb1578e9d44eb909795c2cc6417f5477e
vetemi <valmir.e@gmx.de>
2019-01-28 13:49:20 +0100

    replaced set with list in activity schedules

--- server/src/main/java/de/codeschluss/portal/components/activity/ActivityEntity.java
+++ server/src/main/java/de/codeschluss/portal/components/activity/ActivityEntity.java
@@ -118,7 +118,7 @@ public class ActivityEntity extends BaseResource {
   @OneToMany(fetch = FetchType.LAZY, mappedBy = "activity", cascade = CascadeType.REMOVE)
   @ToString.Exclude
   @JsonIgnore
-  private Set<ScheduleEntity> schedules;
+  private List<ScheduleEntity> schedules;
 
   @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
   @ToString.Exclude

04a2d35e5530a1e6db165e4d3b2efdc777bc98cd
Phil <me@phil.re>
2019-06-17 20:51:44 +0200

    Cosmetic fixes

--- libs/core/utils/base64.ts
+++ libs/core/utils/base64.ts
@@ -1,18 +1,18 @@
 export class Base64 {
 
   public static decode(base64: string): string {
-    switch (true) {
-      case typeof atob === 'function': return atob(base64);
-      case typeof Buffer === 'function':
-        return Buffer.from(base64, 'base64').toString();
+    if (typeof atob === 'function') {
+      return atob(base64);
+    } else if (typeof Buffer === 'function') {
+      return Buffer.from(base64, 'base64').toString();
     }
   }
 
   public static encode(binary: string): string {
-    switch (true) {
-      case typeof btoa === 'function': return btoa(binary);
-      case typeof Buffer === 'function':
-        return Buffer.from(binary, 'binary').toString('base64');
+    if (typeof btoa === 'function') {
+      return btoa(binary);
+    } else if (typeof Buffer === 'function') {
+      return Buffer.from(binary, 'binary').toString('base64');
     }
   }
 

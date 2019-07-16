b530a577075ae8f7ced9762d8dda89a8c0fd65bf
Phil <me@phil.re>
2019-07-16 02:52:17 +0200

    Fix position service

--- libs/core/position/position.service.ts
+++ libs/core/position/position.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
-import { from, Observable, of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class PositionService {
 
   public navigatorPositionResponse(): Observable<Position> {
-    const locator = navigator.geolocation.getCurrentPosition;
+    if (navigator.geolocation.watchPosition) {
+      return new Observable((watch) => {
+        const id = navigator.geolocation.watchPosition(
+          (position) => watch.next(position),
+          (error) => watch.error(error),
+          { enableHighAccuracy: true }
+        );
 
-    if (locator) {
-      return from(new Promise<Position>((resolve, reject) => locator(
-        (position) => resolve(position),
-        (error) => reject(error),
-        { enableHighAccuracy: true }
-      )));
+        return () => navigator.geolocation.clearWatch(id);
+      });
     }
 
     return of(null);

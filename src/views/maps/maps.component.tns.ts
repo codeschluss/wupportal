cda83eb88a7ae84d4a05f5ae3695b1e001985ee3
Phil <me@phil.re>
2019-07-22 01:20:15 +0200

    Hook map loading

--- src/views/maps/maps.component.tns.ts
+++ src/views/maps/maps.component.tns.ts
@@ -1,6 +1,7 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { HttpRequest } from '@angular/common/http';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Route, Router } from '@angular/router';
-import { PlatformProvider, Selfrouter } from '@wooportal/core';
+import { LoadingProvider, PlatformProvider, Selfrouter } from '@wooportal/core';
 import { LoadEventData, WebView } from 'tns-core-modules/ui/web-view';
 import { ClientPackage } from '../../utils/package';
 
@@ -9,7 +10,8 @@ import { ClientPackage } from '../../utils/package';
   templateUrl: 'maps.component.html'
 })
 
-export class MapsComponent extends Selfrouter implements AfterViewInit {
+export class MapsComponent extends Selfrouter
+  implements OnInit, AfterViewInit {
 
   protected routing: Route = {
     path: ''
@@ -23,10 +25,18 @@ export class MapsComponent extends Selfrouter implements AfterViewInit {
   }
 
   public constructor(
+    private loadingProvider: LoadingProvider,
     private platformProvider: PlatformProvider,
     private router: Router
   ) {
     super();
+  }
+
+  public ngOnInit(): void {
+    const request = Object.create(HttpRequest);
+    this.loadingProvider.enqueue(request);
+    this.webview.nativeElement.once('loadFinished', () =>
+      this.loadingProvider.finished(request));
   }
 
   public ngAfterViewInit(): void {
@@ -37,9 +47,10 @@ export class MapsComponent extends Selfrouter implements AfterViewInit {
     if(!wv.nativeView){return wv.once('loaded',()=>this.ngAfterViewInit());}
     // TODO: https://github.com/NativeScript/nativescript-angular/issues/848
 
-    wv.on(WebView.loadStartedEvent, (event: LoadEventData) => {
+    wv.on('loadStarted', (event: LoadEventData) => {
       if (event.url !== this.source) {
-        this.webview.nativeElement.stopLoading();
+        wv.stopLoading();
+        wv.disposeNativeView();
 
         if (event.url.startsWith(url)) {
           this.router.navigateByUrl(event.url.replace(url, ''));

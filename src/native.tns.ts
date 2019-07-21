7c7b527fd414b4bba1f26110465394535683714a
Phil <me@phil.re>
2019-07-21 14:42:43 +0200

    Centralize scss vars/update tns theming

--- src/native.tns.ts
+++ src/native.tns.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformNativeScriptDynamic } from 'nativescript-angular/platform';
+import 'nativescript-theme-core';
 import { NativeModule } from './native.module.tns';
 
 enableProdMode();

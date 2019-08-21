f27c43264485b14811f499c42e4de8f610196a9c
Phil <me@phil.re>
2019-08-21 11:11:01 +0200

    Implement homePage

--- src/views/public/pages/home/home.page.ts
+++ src/views/public/pages/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Route } from '@angular/router';
+import { CrudJoiner, CrudResolver } from '@wooportal/core';
+import { ActivityModel } from '../../../../realm/models/activity.model';
+import { ClientPackage } from '../../../../utils/package';
 import { BasePage } from '../base.page';
 
 @Component({
@@ -9,5 +13,36 @@ import { BasePage } from '../base.page';
 export class HomePageComponent extends BasePage {
 
   protected path: string = '';
+
+  public get activities(): ActivityModel[] {
+    return this.route.snapshot.data.activities;
+  }
+
+  public get title(): string {
+    return ClientPackage.config.defaults.title;
+  }
+
+  protected get routing(): Route {
+    return {
+      path: this.path,
+      resolve: {
+        activities: CrudResolver
+      },
+      data: {
+        resolve: {
+          activities: CrudJoiner.of(ActivityModel, { page: 0, size: 10 })
+            .with('address').yield('suburb')
+            .with('category')
+            .with('schedules')
+        }
+      }
+    };
+  }
+
+  public constructor(
+    private route: ActivatedRoute
+  ) {
+    super();
+  }
 
 }

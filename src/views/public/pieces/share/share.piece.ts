a0497e720b762092e94ed021d441340244e7f558
Phil <me@phil.re>
2019-08-26 12:19:53 +0200

    Add share via link/mail

--- src/views/public/pieces/share/share.piece.ts
+++ src/views/public/pieces/share/share.piece.ts
@@ -11,6 +11,7 @@ import { BasePiece } from '../base.piece';
 interface Service {
   name: string;
   icon: string;
+  pack: string;
   url: string;
 }
 
@@ -24,26 +25,47 @@ export class SharePieceComponent extends BasePiece {
 
   public items: Service[] = [
     {
+      name: 'E-Mail',
+      icon: 'envelope',
+      pack: 'fas',
+      url: 'mailto:?body='
+    },
+    {
       name: 'Facebook',
       icon: 'facebook-f',
+      pack: 'fab',
       url: 'https://www.facebook.com/sharer/sharer.php?u='
     },
     {
       name: 'Telegram',
       icon: 'telegram-plane',
+      pack: 'fab',
       url: 'https://telegram.me/share/url?url='
     },
     {
       name: 'Twitter',
       icon: 'twitter',
+      pack: 'fab',
       url: 'https://twitter.com/share?url='
     },
     {
       name: 'WhatsApp',
       icon: 'whatsapp',
+      pack: 'fab',
       url: 'https://wa.me/?text='
     }
   ];
+
+  public get href(): string {
+    return ClientPackage.config.defaults.appUrl + (() => {
+      switch (this.item.constructor) {
+        case ActivityModel: return '/activities/';
+        case BlogModel: return '/blogposts/';
+        case OrganisationModel: return '/organisations/';
+        case PageModel: return '/infopages/';
+      }
+    })() + this.item.id;
+  }
 
   public constructor(
     @Inject(DOCUMENT) private document: Document,
@@ -52,21 +74,31 @@ export class SharePieceComponent extends BasePiece {
     super();
   }
 
-  public share(service: Service): void {
-    const url = ClientPackage.config.defaults.appUrl + (() => {
-      switch (this.item.constructor) {
-        case ActivityModel: return '/activities/';
-        case BlogModel: return '/blogposts/';
-        case OrganisationModel: return '/organisations/';
-        case PageModel: return '/infopages/';
-      }
-    })() + this.item.id;
-
+  public copy(): void {
     switch (this.platformProvider.type) {
       case 'Native':
         break;
       case 'Online':
-        this.document.defaultView.open(service.url + url, '_blank');
+        const clipboard = this.document.createElement('textarea');
+        clipboard.style.opacity = '0';
+        clipboard.style.position = 'absolute';
+        clipboard.value = this.href;
+        this.document.body.appendChild(clipboard);
+
+        clipboard.focus();
+        clipboard.select();
+        this.document.execCommand('copy');
+        this.document.body.removeChild(clipboard);
+        break;
+    }
+  }
+
+  public share(service: Service): void {
+    switch (this.platformProvider.type) {
+      case 'Native':
+        break;
+      case 'Online':
+        this.document.defaultView.open(service.url + this.href, '_blank');
         break;
     }
   }

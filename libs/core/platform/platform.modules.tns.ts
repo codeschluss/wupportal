8e7bef0a5e10404e58ed5a7670c2f8bedc67fb00
Phil <me@phil.re>
2019-09-02 17:50:13 +0200

    Fix re-export of platform modules

--- libs/core/platform/platform.modules.tns.ts
+++ libs/core/platform/platform.modules.tns.ts
@@ -1,2 +1,3 @@
 export { NativeScriptCommonModule as PlatformCommonModule } from 'nativescript-angular/common';
 export { NativeScriptRouterModule as PlatformRouterModule } from 'nativescript-angular/router';
+export { fromBase64 } from 'tns-core-modules/image-source';
--- libs/core/platform/platform.modules.ts
+++ libs/core/platform/platform.modules.ts
@@ -1,2 +1,5 @@
+const fromBase64 = void 0;
+
 export { CommonModule as PlatformCommonModule } from '@angular/common';
 export { RouterModule as PlatformRouterModule } from '@angular/router';
+export { fromBase64 };

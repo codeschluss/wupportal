66b16a8a5fe9a137a765df107d6b2b83ba2d40c3
Phil <me@phil.re>
2019-09-08 10:47:16 +0200

    Implement/overhaul tns cards

--- src/views/public/cards/blogpost/blogpost.card.ts
+++ src/views/public/cards/blogpost/blogpost.card.ts
@@ -1,7 +1,4 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, Inject } from '@angular/core';
-import { Router } from '@angular/router';
-import { PlatformProvider } from '@wooportal/core';
+import { Component } from '@angular/core';
 import * as marked from 'marked';
 import { BlogModel } from '../../../../realm/models/blog.model';
 import { BaseCard } from '../base.card';
@@ -15,32 +12,13 @@ import { BaseCard } from '../base.card';
 export class BlogpostCardComponent extends BaseCard<BlogModel> {
 
   public get preview(): string {
-    const content = marked(this.item.content).replace(/<[^>]*>/g, '');
-    let preview = '';
-
-    switch (this.platformProvider.type) {
-      case 'Native':
-        // TODO: native html decoding
-        break;
-
-      case 'Online':
-        const decoder = this.document.createElement('textarea');
-        decoder.innerHTML = content;
-        preview = decoder.value;
-        break;
-    }
+    const preview = marked(this.item.content)
+      .replace(/&#\d+;/g, (i) => String.fromCharCode(i.match(/\d+/g)[0]))
+      .replace(/<[^>]*>/g, '').replace(/\s+/g, ' ');
 
     return preview.length > 500
       ? preview.substr(0, 500) + '...'
       : preview;
-  }
-
-  public constructor(
-    @Inject(DOCUMENT) private document: Document,
-    private platformProvider: PlatformProvider,
-    router: Router,
-  ) {
-    super(router);
   }
 
 }

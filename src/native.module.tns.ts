4b3fd53660eb730e1ae8e741135d8f2a5954b123
Phil <me@phil.re>
2019-09-14 09:07:42 +0200

    Implement native storage interface

--- src/native.module.tns.ts
+++ src/native.module.tns.ts
@@ -2,6 +2,7 @@ import { ErrorHandler, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import { LocalDatabase } from '@ngx-pwa/local-storage';
 import { CoreModule } from '@wooportal/core';
 import { NativeScriptAnimationsModule } from 'nativescript-angular/animations';
 import { NativeScriptHttpClientModule } from 'nativescript-angular/http-client';
@@ -12,6 +13,7 @@ import { NativeComponent } from './native.component.tns';
 import { NativeRouter } from './native.router.tns';
 import { RealmModule } from './realm/realm.module';
 import { ClientPackage } from './utils/package';
+import { NativeStorageDatabase } from './utils/storage';
 
 @NgModule({
   bootstrap: [NativeComponent],
@@ -33,7 +35,8 @@ import { ClientPackage } from './utils/package';
     RealmModule
   ],
   providers: [
-    { provide: ErrorHandler, useClass: ClientErrorHandler }
+    { provide: ErrorHandler, useClass: ClientErrorHandler },
+    { provide: LocalDatabase, useClass: NativeStorageDatabase }
   ],
   schemas: [NO_ERRORS_SCHEMA]
 })
---
+++ src/utils/storage.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { LocalDatabase, StorageMap } from '@ngx-pwa/local-storage';
+import { asyncScheduler, Observable, of, throwError } from 'rxjs';
+import { observeOn } from 'rxjs/operators';
+import * as nativeStorage from 'tns-core-modules/application-settings';
+
+@Injectable({
+  providedIn: StorageMap
+})
+
+export class NativeStorageDatabase implements LocalDatabase {
+
+  public get size(): Observable<number> {
+    return of(nativeStorage.getAllKeys().length);
+  }
+
+  public clear(): Observable<undefined> {
+    nativeStorage.clear();
+    return of(undefined);
+  }
+
+  public delete(key: string): Observable<undefined> {
+    nativeStorage.remove(key);
+    return of(undefined);
+  }
+
+  public get<T = any>(key: string): Observable<T | undefined> {
+    const data = nativeStorage.getString(key);
+
+    if (data === null || data === undefined) {
+      return of(undefined);
+    }
+
+    try {
+      return of(JSON.parse(data) as T);
+    } catch (error) {
+      return throwError(error as SyntaxError);
+    }
+  }
+
+  public has(key: string): Observable<boolean> {
+    return nativeStorage.hasKey(key) ? of(true) : of(false);
+  }
+
+  public keys(): Observable<string> {
+    return new Observable<string>((observer) => {
+      nativeStorage.getAllKeys().forEach((key) => observer.next(key));
+      observer.complete();
+    }).pipe(observeOn(asyncScheduler));
+  }
+
+  public set(key: string, data: any): Observable<undefined> {
+    let json: string | null = null;
+
+    try {
+      json = JSON.stringify(data);
+    } catch (error) {
+      return throwError(error as TypeError);
+    }
+
+    try {
+      nativeStorage.setString(key, json);
+      return of(undefined);
+    } catch (error) {
+      return throwError(error as DOMException);
+    }
+  }
+
+}

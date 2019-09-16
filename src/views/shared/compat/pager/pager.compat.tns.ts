94181176b9aefdca59d0609abd18b61cd174bb71
Phil <me@phil.re>
2019-09-16 17:49:18 +0200

    Implement tns pager

--- src/views/shared/compat/pager/pager.compat.tns.ts
+++ src/views/shared/compat/pager/pager.compat.tns.ts
@@ -1,5 +1,5 @@
 import { Component, HostBinding, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { isKnownView, registerElement } from 'nativescript-angular/element-registry';
 import { ContentView } from 'tns-core-modules/ui/page';
 import { PagerCompat } from './pager.compat.i';
@@ -11,7 +11,32 @@ if (!isKnownView('pager-compat')) {
 @Component({
   selector: 'pager-compat',
   styleUrls: ['pager.compat.scss'],
-  template: ``
+  template: `
+  <StackLayout>
+    <AbsoluteLayout class="prev">
+      <i18n i18n="@@previous" unit="previous"></i18n>
+      <icon-compat ripple
+        icon="angle-double-left"
+        [isEnabled]="prev"
+        [isUserInteractionEnabled]="prev"
+        [ngClass]="{ disabled: !prev }"
+        (tap)="router.navigate([], { queryParams: prevLink })">
+      </icon-compat>
+      <GridLayout></GridLayout>
+    </AbsoluteLayout>
+    <AbsoluteLayout class="next">
+      <i18n i18n="@@following" unit="following"></i18n>
+      <icon-compat ripple
+        icon="angle-double-right"
+        [isEnabled]="next"
+        [isUserInteractionEnabled]="next"
+        [ngClass]="{ disabled: !next }"
+        (tap)="router.navigate([], { queryParams: nextLink })">
+      </icon-compat>
+      <GridLayout></GridLayout>
+    </AbsoluteLayout>
+  </StackLayout>
+  `
 })
 
 export class PagerCompatComponent implements PagerCompat {
@@ -38,6 +63,7 @@ export class PagerCompatComponent implements PagerCompat {
   }
 
   public constructor(
+    public router: Router,
     private route: ActivatedRoute
   ) { }
 
